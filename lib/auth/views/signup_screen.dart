1ad1979c94dea0193e0d68f6a30d0453c1e8476f
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2023-12-09 14:24:18 +0100

    Rearrange import statements

--- lib/auth/views/signup_screen.dart
+++ lib/auth/views/signup_screen.dart
@@ -1,13 +1,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:todo/auth/models/status.dart';
 import 'package:todo/auth/view_models/auth_view_model.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:todo/validation/view_models/signup_validation.dart';
-
-import '../models/status.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
