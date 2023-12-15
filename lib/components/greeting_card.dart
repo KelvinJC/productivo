3f353b1595dddfcf39e7ba0886bbe532a64b62a5
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2023-12-15 16:10:10 +0100

    Move date formatting logic to clock viewmodel, place real-time clock & date in card

--- lib/components/greeting_card.dart
+++ lib/components/greeting_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/clock/view_models/clock_view_model.dart';
 
 
 
@@ -9,32 +9,19 @@ class GreetUserCard extends StatelessWidget {
 
   const GreetUserCard({super.key});
 
-  String appropriateGreeting (currentTime) {
-    String formattedTime = DateFormat.Hms().format(currentTime);
-    String hourOfDay = formattedTime.split(':')[0];
-
-    switch (int.parse(hourOfDay)) {
-      case < 12 && >= 0:      // 00:00 am - 11:59 am
-        return 'Morning';
-      case == 12:             // 12:00 pm - 12:59 pm
-        return 'Noon';
-      case < 16 && > 12:      // 13:00 pm - 15:59 pm
-        return 'Afternoon';
-      case >= 16 && <= 19:    // 16:00 pm - 19:59 pm
-        return 'Evening';
-      default:                // 20:00 pm - 23:59 pm
-        return 'Night';
-    }
+  String abbreviateMonth(String date) {
+    List splitDate = date.split(' ');
+    String monthAbbreviated = splitDate[0].substring(0,3);
+    return '$monthAbbreviated ${splitDate[1]}, ${splitDate[2]}';
   }
 
   @override
   Widget build(BuildContext context) {
-    DateTime _currentTime = DateTime.now();
-    String formattedTime = DateFormat.Hm().format(_currentTime);
-    String formattedDate = DateFormat('EEEE, MMMM d y').format(_currentTime);
-    List splitFormattedDate = formattedDate.split(', ');
-    String currentDay = splitFormattedDate[0];
-    String currentDate = splitFormattedDate[1];
+    ClockViewModel clockViewModel = context.watch<ClockViewModel>();
+    String currentTime = clockViewModel.formattedTime;
+    String currentDay = clockViewModel.formattedDay;
+    String currentDate = clockViewModel.formattedDate;
+    String periodOfDay = clockViewModel.periodOfDay;
 
     return Container(
       width: 370,
@@ -58,7 +45,7 @@ class GreetUserCard extends StatelessWidget {
             ),
           ),
           Text(
-            appropriateGreeting(_currentTime),
+            periodOfDay,
             style: GoogleFonts.montserrat(
                 fontWeight: FontWeight.bold,
                 fontSize: 48,
@@ -70,7 +57,7 @@ class GreetUserCard extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                'Today  ',//$currentDay',
+                currentTime,
                 style: GoogleFonts.montserrat(
                     fontWeight: FontWeight.bold,
                     fontSize: 18,
@@ -78,20 +65,20 @@ class GreetUserCard extends StatelessWidget {
                 ),
               ),
               Text(
-                'Completed',
+                'Score',
                 style: GoogleFonts.montserrat(
                     fontWeight: FontWeight.bold,
                     fontSize: 18,
                     color: Colors.white70
                 ),
-              )
+              ),
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                currentDate,
+                abbreviateMonth(currentDate),
                 style: GoogleFonts.montserrat(
                   // fontWeight: FontWeight.bold,
                     fontSize: 14,
@@ -99,13 +86,13 @@ class GreetUserCard extends StatelessWidget {
                 ),
               ),
               Text(
-                '75%',
+                '1,075',
                 style: GoogleFonts.montserrat(
                   // fontWeight: FontWeight.bold,
                     fontSize: 14,
                     color: Colors.white70
                 ),
-              )
+              ),
             ],
           ),
         ],
