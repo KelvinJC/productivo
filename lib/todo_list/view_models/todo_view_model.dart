0248f2f18b5b3f07bc2f8876d3c9be7f96a002de
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2023-12-21 19:59:33 +0100

    Implement toggle between time and all day

--- lib/todo_list/view_models/todo_view_model.dart
+++ lib/todo_list/view_models/todo_view_model.dart
@@ -3,10 +3,15 @@ import 'package:flutter/material.dart';
 class TodoViewModel extends ChangeNotifier{
   bool _isStartDateCalendarVisible = false;
   bool _isEndDateCalendarVisible = false;
+  bool _isTimeSelected = true;
+  bool _isAllDay = false;
 
   // getters
   bool get isStartDateCalendarVisible => _isStartDateCalendarVisible;
   bool get isEndDateCalendarVisible => _isEndDateCalendarVisible;
+  bool get isTimeSelected => _isTimeSelected;
+  bool get isAllDay => _isAllDay;
+
 
   // setters
   void toggleStartDateCalendarVisible() {
@@ -19,6 +24,24 @@ class TodoViewModel extends ChangeNotifier{
     _isStartDateCalendarVisible = false;
     _isEndDateCalendarVisible = !_isEndDateCalendarVisible;
     notifyListeners();
+  }
+
+  void selectTime() {
+    if (!_isTimeSelected) {
+      _isTimeSelected = true;
+      _isAllDay = false;
+    }
+    notifyListeners();
+
+  }
+
+  void selectAllDay() {
+    if (!_isAllDay) {
+      _isAllDay = true;
+      _isTimeSelected = false;
+    }
+    notifyListeners();
+
   }
 
 
