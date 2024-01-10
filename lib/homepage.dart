8ac57b4eb20d1b4fafbcd31a14b01967bd2182da
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-01-10 19:03:37 +0100

    Use staggered view on home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,8 +6,68 @@ import 'package:todo/components/greeting_card.dart';
 import 'package:todo/components/todo_list_row.dart';
 import 'package:todo/validation/view_models/login_validation.dart';
 import 'auth/view_models/auth_view_model.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
+class MyTileWidget extends StatelessWidget {
+  final Color color;
+  final int index;
+  final String text;
+  final String completed;
 
+  MyTileWidget(this.index, this.color, this.text, this.completed);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: color,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: [
+                Text(
+                  text,
+                  style: GoogleFonts.montserrat(
+                    fontSize: 15,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.bold,
+                    // shadows: [
+                    //   const Shadow(
+                    //     offset: Offset(0.0, 0.8), // Set the offset of the shadow
+                    //     blurRadius: 2.0, // Set the blur radius of the shadow
+                    //     color: Colors.grey, // Set the color of the shadow
+                    //   ),
+                    // ],
+                  ),
+                ),
+                Text(
+                  completed,
+                  style: GoogleFonts.montserrat(
+                      fontSize: 15,
+                      color: Colors.white70,
+                      // fontWeight: FontWeight.bold
+                  ),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(135.0,1,0.0,0.0),
+              child: Icon(
+                Icons.add,
+                color: Colors.white70,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,7 +76,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     FirebaseAuthViewModel authViewModel = context.watch<FirebaseAuthViewModel>();
     LoginValidationViewModel loginValidationViewModel = context.watch<LoginValidationViewModel>();
+    List colors = [
+      (Colors.grey[350]),
+      (Colors.grey),
+      (Colors.grey[850]),
+      (Colors.black38),
+      (Colors.brown[200]),
+      (Colors.blueGrey[100]),
+      (Colors.grey),
+      (Colors.grey[350]),
+      (Colors.black),
+    ];
 
+    List text = [
+      'Personal',
+      'Sports',
+      'Work',
+      'Social',
+      'Projects',
+      'School',
+      'Study',
+      'Purchase',
+      'Entertainment',
+      'Finance',
+    ];
+
+    List  completedTasks = [
+      '11',
+      '50',
+      '24',
+      '2',
+      '4',
+      '0',
+      '1',
+      '78'
+    ];
 
     return Scaffold(
       backgroundColor: Colors.grey[200],
@@ -24,13 +118,13 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.grey[200],
         surfaceTintColor: Colors.transparent,
         leading: Padding(
-          padding: EdgeInsets.only(left: 20.0),
+          padding: const EdgeInsets.only(left: 20.0),
           child: GestureDetector(
             onTap: () {
               loginValidationViewModel.changeUserLoggedOut();
               authViewModel.signOut();
             },
-            child: CircleAvatar(
+            child: const CircleAvatar(
               radius: 50,
               backgroundImage: AssetImage('assets/images/beach3.png'),
             ),
@@ -42,8 +136,8 @@ class HomePage extends StatelessWidget {
             child: Text(
               'Hi Kelvin',
               style: GoogleFonts.montserrat(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
+                fontSize: 20,
+                fontWeight: FontWeight.bold
               ),
             ),
           ),
@@ -107,68 +201,84 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           GreetUserCard(),
-          SizedBox(height: 25,),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0,),
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '8 Tasks',
-                      style: GoogleFonts.montserrat(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold // normal if unselected
-                      ),
-                    ),
-                    Text(
-                      '4 Boards',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 15,
-                        // fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  ],
-                ),
-
-                // tab switch imitation
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: 180,
-                        child: Divider(color: Colors.black,) // switches colour based on selection of tasks or boards
-                    ),
-                    SizedBox(
-                        width: 180,
-                        child: Divider(color: Colors.grey[400],)
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-          SizedBox(height: 20,),
+          // SizedBox(height: 25,),
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 25.0,),
+          //   child: Column(
+          //     // mainAxisAlignment: MainAxisAlignment.start,
+          //     children: [
+          //       Row(
+          //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //         children: [
+          //           Text(
+          //             '8 Tasks',
+          //             style: GoogleFonts.montserrat(
+          //                 fontSize: 15,
+          //                 fontWeight: FontWeight.bold // normal if unselected
+          //             ),
+          //           ),
+          //           Text(
+          //             '4 Boards',
+          //             style: GoogleFonts.montserrat(
+          //               fontSize: 15,
+          //               // fontWeight: FontWeight.bold
+          //             ),
+          //           ),
+          //         ],
+          //       ),
+          //
+          //       // tab switch imitation
+          //       Row(
+          //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //         children: [
+          //           const SizedBox(
+          //             width: 180,
+          //               child: Divider(color: Colors.black,) // switches colour based on selection of tasks or boards
+          //           ),
+          //           SizedBox(
+          //               width: 180,
+          //               child: Divider(color: Colors.grey[400],)
+          //           ),
+          //         ],
+          //       )
+          //     ],
+          //   ),
+          // ),
+          const SizedBox(height: 12,),
           Expanded(
-            child: ListView.separated(
-                padding: EdgeInsets.only(top: 5.0, bottom: 15.0),
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: TodoListRow(index: index),
-                  );
-                },
-                separatorBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Divider(
-                    color: Colors.grey[200],
-                  ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: StaggeredGridView.countBuilder(
+                crossAxisCount: 2, // Number of columns
+                itemCount: 8, // Number of items
+                itemBuilder: (BuildContext context, int index) => MyTileWidget(index, colors[index], text[index], completedTasks[index]),
+                staggeredTileBuilder: (int index) => StaggeredTile.count(
+                  1, // Width of the tile
+                  index.isEven ? 1.2 : 0.9, // Height of the tile
                 ),
-                itemCount: 10
+                mainAxisSpacing: 8.0, // Spacing between tiles vertically
+                crossAxisSpacing: 8.0, // Spacing between tiles horizontally
+              ),
             ),
           ),
+          // Expanded(
+          //   child: ListView.separated(
+          //       padding: EdgeInsets.only(top: 5.0, bottom: 15.0),
+          //       itemBuilder: (context, index) {
+          //         return Padding(
+          //           padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          //           child: TodoListRow(index: index),
+          //         );
+          //       },
+          //       separatorBuilder: (context, index) => Padding(
+          //         padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          //         child: Divider(
+          //           color: Colors.grey[200],
+          //         ),
+          //       ),
+          //       itemCount: 10
+          //   ),
+          // ),
 
         ],
       ),
