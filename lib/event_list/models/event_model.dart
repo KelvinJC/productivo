5fc2f35c342c887ec1fd04cac789ec5fbf056dd6
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-01-30 14:14:51 +0100

    Add fromMap method, extra attributes to constructor and string representation

--- lib/event_list/models/event_model.dart
+++ lib/event_list/models/event_model.dart
@@ -1,4 +1,7 @@
+
+
 class Event {
+  int? id;
   final String title;
   final String start;
   final String end;
@@ -9,16 +12,22 @@ class Event {
   String? priority;
 
   Event({
+    this.id,
     required this.title,
     required this.start,
     required this.end,
     required this.category,
+    this.note,
+    this.reminder,
+    this.location,
+    this.priority,
   });
 
   // Convert an event into a Map. The keys must correspond to the names of the
   // columns in the database.
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'title': title,
       'start': start,
       'end': end,
@@ -30,10 +39,22 @@ class Event {
     };
   }
 
+  Event.fromMap(Map<String, dynamic> data) :
+    id = data['id'],
+    title = data['title'],
+    start = data['start'],
+    end = data['end'],
+    category = data['category'],
+    note = data['note'],
+    reminder = data['reminder'],
+    location = data['location'],
+    priority = data['priority'];
+
+
   // Implement toString to make it easier to see information about
   // each event when using the print statement.
   @override
   String toString() {
-    return 'Event{title: $title, start: $start, end: $end, category: $category}';
+    return 'Event{id: $id, title: $title, start: $start, end: $end, category: $category}';
   }
 }
